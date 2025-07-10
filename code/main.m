c5106a6999a307c0746185b10789c3a465642a62
pedgomgal1 <pgomez-ibis@us.es>
2025-07-10 17:47:10 +0200

    updated features saving

--- code/main.m
+++ code/main.m
@@ -13,6 +13,8 @@ extractedFeaturesPath = fullfile('..','results','extractedFeatures');
 listOfImageNames = dir(fullfile(inferredImagesPath, '*.tif'));
 nOfImages = size(listOfImageNames,1);
 
+imageResFactor = 0.2525251; %pixel width in um
+
 for nImg =1:nOfImages
 
     imageName = listOfImageNames(nImg).name;
@@ -39,15 +41,17 @@ for nImg =1:nOfImages
     imNuclei=imresize(imNuclei,size(rawImg),'nearest');
     
 
-    % if exist(fullfile(extractedFeaturesPath,strrep(imageName,'.tif','.mat')),'file')
-    %     load(fullfile(extractedFeaturesPath,strrep(imageName,'.tif','.mat')))
-    %     allImgsParams(nImg) = myotubesParams;
-    % else
+    if exist(fullfile(extractedFeaturesPath,strrep(imageName,'.tif','.mat')),'file')
+        load(fullfile(extractedFeaturesPath,strrep(imageName,'.tif','.mat')))
+        myotubesParams.totalSkelLength = sum(BW_skel(:)==1)*imageResFactor;
+        allImgsParams(nImg) = myotubesParams;
+    else
         [myotubesParams,nucleiIng_filt,nucleiClusters] = myotubes_analysis(BW_clean, BW_skel,imNuclei,rawImg);
         myotubesParams.fileName = imageName;
+        myotubesParams.totalSkelLength = sum(BW_skel(:)==1)*imageResFactor;
         allImgsParams(nImg) = myotubesParams;
         save(fullfile(extractedFeaturesPath,strrep(imageName,'.tif','.mat')),'myotubesParams','nucleiClusters','nucleiIng_filt')
-    % end
+    end
 
     disp([num2str(nImg) '/' num2str(nOfImages) ' loaded - ' imageName])
     
@@ -55,7 +59,15 @@ for nImg =1:nOfImages
 end
 
 T = struct2table(allImgsParams);
-writetable(T,fullfile(extractedFeaturesPath,['allImagesFeatures_' date '.xlsx']))
+T_res = T(:,[25,1:14,26,15:24]);
+T_res(:,[2:10,12:16,19])=T_res(:,[2:10,12:16,19]).*imageResFactor;
+T_res(:,26)=T_res(:,26).*(imageResFactor^2);
+T_res.overallRatioAreaLength = T_res.percMyoArea.*T_res.areaImage./T_res.totalSkelLength;
 
 
-    
+writetable(T_res,fullfile(extractedFeaturesPath,['features_perFibreIncluded_' date '.xlsx']))
+
+T_wholeImg = T_res(:,[1,4,9,10,16,18,27,20:26]);
+T_wholeImg.largestConnectedSkel = T_res.maxSkelLength;
+writetable(T_wholeImg,fullfile(extractedFeaturesPath,['features_wholeImg_' date '.xlsx']))
+
